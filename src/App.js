8e8ebfb1550d84af88f58314a20e13b466c1f7f6
PinguD <daanmeerhof@gmail.com>
2021-09-06 12:20:46 +0200

    fixed warningss

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-import Map from './components/Map'
+import "./App.css";
+import Map from "./components/Map";
 
 function App() {
   return (
     <div className="App">
-     <p>placeholder</p>
-      <Map/>
+      <p>placeholder</p>
+      <Map />
     </div>
   );
 }
--- src/components/Map.js
+++ src/components/Map.js
@@ -1,25 +1,28 @@
-export default function Map(){
-
-    const data = [
-        {
-            name: "Banana"
-        },
-        {
-            name: "Apple"
-        },
-        {
-            name: "Orange"
-        },
-        {
-            name: "Watermelon"
-        }
-    ]
-    return (
-        <div>
-            {data.map(fruit => {
-            return <div> <p>{fruit.name}</p>
-            </div>
-            })}
-        </div>
-    )
+export default function Map() {
+  const data = [
+    {
+      name: "Banana",
+    },
+    {
+      name: "Apple",
+    },
+    {
+      name: "Orange",
+    },
+    {
+      name: "Watermelon",
+    },
+  ];
+  return (
+    <div>
+      {data.map((fruit, index) => {
+        return (
+          <div key={index}>
+            {" "}
+            <p>{fruit.name}</p>
+          </div>
+        );
+      })}
+    </div>
+  );
 }
